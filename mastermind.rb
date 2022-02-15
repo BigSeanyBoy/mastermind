1186b7c8f92bc99d703ad0ed8d769ecc1f1ea1c2
Sean Hammell <sean.m.hammell@gmail.com>
2022-02-15 12:56:38 -0500

    Add #player_maker and #player_breaker

--- mastermind.rb
+++ mastermind.rb
@@ -5,7 +5,7 @@
 class Mastermind
   attr_reader :num_guesses
 
-  def initialize(code = Array.new(4) { rand(6).to_s }, num_guesses = 12)
+  def initialize(code, num_guesses)
     @code = code
     @num_guesses = num_guesses
   end
@@ -47,8 +47,40 @@ class Mastermind
   end
 end
 
-def main
-  mastermind = Mastermind.new(Array.new(4) { rand(6).to_s }, 6)
+# The Computer class represents the opponent of the
+# user; the codebreaker of the user's code.
+class Computer
+  def initialize
+    0
+  end
+
+  def guess
+    puts "\nComputer guess: #{@code_set[0]}"
+    print 'Enter number of black keys >>> '
+    black_keys = gets.chomp.to_i
+    return true if black_keys == 4
+
+    print 'Enter number of white keys >>> '
+    white_keys = gets.chomp.to_i
+    update_set(black_keys, white_keys)
+  end
+
+  def update_set(black_keys, white_keys)
+    @code_set.shuffle!
+  end
+end
+
+def pregame_settings
+  puts "\nHello! Welcome to Mastermind!"
+  print "\nWould you like to be the codemaker [0] or the codebreaker [1]? >>> "
+  player_role = gets.chomp.to_i
+  print 'Select the difficulty: expert [0], hard [1], medium [2], easy [3] >>> '
+  difficulty = gets.chomp.to_i
+  [player_role, difficulty]
+end
+
+def player_breaker(difficulty)
+  mastermind = Mastermind.new(Array.new(4) { rand(6).to_s }, difficulty)
 
   mastermind.num_guesses.times do
     print "\nEnter your guess >> "
@@ -60,6 +92,24 @@ def main
   end
 
   'You lose...'
+end
+
+def player_maker(difficulty)
+  print "\nEnter the code >>> "
+  code = gets.chomp.to_i
+  mastermind = Mastermind.new(code, difficulty)
+  computer = Computer.new
+
+  mastermind.num_guesses.times do  
+    return 'Computer Wins...' if computer.guess == true
+  end
+  'You win!'
+end
+
+def main
+  settings = pregame_settings
+  difficulty = 6 + (settings[1] * 2)
+  settings[0] == 1 ? player_breaker(difficulty) : player_maker(difficulty)
 end
 
 puts main
--- spec/mastermind_spec.rb
+++ spec/mastermind_spec.rb
@@ -1,31 +1,32 @@
-require './mastermind.rb'
+# frozen_string_literal: true
+
+require './mastermind'
 
 mastermind = Mastermind.new
-mastermind.assign_code(['1', '1', '2', '2'])
+mastermind.assign_code(%w[1 1 2 2])
 
 RSpec.describe 'Mastermind Tests' do
   describe '#check_code tests' do
-    
     it 'no keys' do
-      keys = mastermind.check_code(['3', '4', '5', '3'])
+      keys = mastermind.check_code(%w[3 4 5 3])
       result = [0, 0]
       expect(keys).to eq(result)
     end
 
     it 'four black' do
-      keys = mastermind.check_code(['1', '1', '2', '2'])
+      keys = mastermind.check_code(%w[1 1 2 2])
       result = [4, 0]
       expect(keys).to eq(result)
     end
 
     it 'four white' do
-      keys = mastermind.check_code(['2', '2', '1', '1'])
+      keys = mastermind.check_code(%w[2 2 1 1])
       result = [0, 4]
       expect(keys).to eq(result)
     end
 
     it 'two each' do
-      keys = mastermind.check_code(['1', '2', '1', '2'])
+      keys = mastermind.check_code(%w[1 2 1 2])
       result = [2, 2]
       expect(keys).to eq(result)
     end
